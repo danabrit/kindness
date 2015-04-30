316fd31cd57b063d8d61046011b3936955fe629c
Dana Jones <djones@larkfarm.com>
2015-04-30 17:10:41 -0500

    Refactored Act#in_units function to be three different functions
    Each function is specific to the type of unit it is interpreting

--- app/models/act.rb
+++ app/models/act.rb
@@ -1,26 +1,27 @@
 class Act < ActiveRecord::Base
-  def in_units(kind_of_cost, measurement)
-    case kind_of_cost
-    when "cost"
-      "$" * measurement
-    when "effort"
-      case measurement
-      when 1
-        "Easy"
-      when 2
-        "Medium"
-      when 3
-        "Hard"
-      end
-    when "time"
-      case measurement
-      when 1
-        "Less than one hour"
-      when 2
-        "1-3 hours"
-      when 3
-        "More than 3 hours"
-      end
+  def cost_units(measurement)
+    "$" * measurement
+  end
+
+  def effort_units(measurement)
+    case measurement
+    when 1
+      "Easy"
+    when 2
+      "Medium"
+    when 3
+      "Hard"
+    end    
+  end
+
+  def time_units(measurement)
+    case measurement
+    when 1
+      "Less than one hour"
+    when 2
+      "1-3 hours"
+    when 3
+      "More than 3 hours"
     end
   end
 end
